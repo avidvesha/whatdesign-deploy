822086902974933a6fb1f1638d00b0c5e037c639
Chris-adit <adityachris01@students.amikom.ac.id>
2026-06-16 22:11:50 +0700

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,9 @@ import { createRoot } from "react-dom/client";
 // @ts-ignore: CSS imports require type declarations in this setup
 import "./index.css";
 import App from "./App";
-import loadAnalytics from "./utils/load-analytics";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 );
-
-loadAnalytics();
