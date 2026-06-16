feaa9e633ff81b65b1049e6e75b0f1ca2b3f5686
avidvesha <apit.scorpio10@gmail.com>
2026-06-16 13:28:38 +0700

    feat: add google analytics, fix minor bugs

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+// @ts-ignore: CSS imports require type declarations in this setup
 import "./index.css";
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
 );
