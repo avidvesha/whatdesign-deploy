bd7d98a3ff75ba1bdb7f1520fcacc574667a5533
avidvesha <apit.scorpio10@gmail.com>
2026-06-27 19:40:44 +0700

    add: va speed insight

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Testimonials from "./components/Testimonials";
 import CTA from "./components/CTA";
 import Footer from "./components/Footer";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 
 export default function App() {
@@ -23,6 +24,7 @@ export default function App() {
       <CTA />
       <Footer />
       <Analytics />
+      <SpeedInsights />
     </div>
   );
 }
