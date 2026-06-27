6cfb7868a999a8031008da26dfa4c4cd1e17847c
avidvesha <apit.scorpio10@gmail.com>
2026-06-27 19:15:46 +0700

    delete: ga4 unused files

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,10 @@ import FAQ from "./components/FAQ";
 import Testimonials from "./components/Testimonials";
 import CTA from "./components/CTA";
 import Footer from "./components/Footer";
-import GoogleAnalytics from "./GoogleAnalytics";
 
 export default function App() {
   return (
     <div className="min-h-screen bg-white">
-      <GoogleAnalytics measurementId={import.meta.env.VITE_GOOGLE_ANALYTICS_TRACKING_ID || "G-926BQSVSCN"} />
       <Navbar />
       <Hero />
       <ClientLogos />
--- src/utils/google.d.ts
+++
@@ -1,5 +0,0 @@
-// global.d.ts
-interface Window {
-  dataLayer: any[];
-  gtag: (...args: any[]) => void;
-}
