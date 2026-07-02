939733722527f6c59f304e226668d304fabc1da4
avidvesha <apit.scorpio10@gmail.com>
2026-07-02 12:55:12 +0700

    fix: ga installation 1.2

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,6 @@ import { createRoot } from "react-dom/client";
 // @ts-ignore: CSS imports require type declarations in this setup
 import "./index.css";
 import App from "./App";
-import { initGA } from './utils/analytics';
-
-initGA()
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
