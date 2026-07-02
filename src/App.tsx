fcc4268c161811fbefb639a0a55637912dea9f3f
avidvesha <apit.scorpio10@gmail.com>
2026-07-02 12:44:15 +0700

    fix: ga installation

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,8 @@ import FAQ from "./components/FAQ";
 import Testimonials from "./components/Testimonials";
 import CTA from "./components/CTA";
 import Footer from "./components/Footer";
-import ReactGA from 'react-ga4';
-import { useEffect } from "react";
-
 
 export default function App() {
-  useEffect(() => {
-    ReactGA.initialize('G-RWMELF3TM3');
-    ReactGA.send({
-      hitType: "pageview",
-      page: window.location.pathname,
-      title: 'App.tsx'
-    });
-  }, []);
   return (
     <div className="min-h-screen bg-white">
       <Navbar />
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_GOOGLE_ANALYTICS_TRACKING_ID?: string;
+  readonly VITE_GA_MEASUREMENT_ID?: string;
 }
 
 interface ImportMeta {
